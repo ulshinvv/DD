b6dcf000f03d2f3d94896ee25371a3bdaec4dd7b
ulshinvv <158988350+ulshinvv@users.noreply.github.com>
2024-11-27 20:43:25 +0700

    Add files via upload

---
+++ TurOgenstvo/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TurOgenstvo.model;
+
+namespace TurOgenstvo
+{
+    internal class MyDbContext: DbContext
+    {
+        public string connectionString = @"Data Source=VOVA; Initial Catalog=ttt3t; Integrated Security=true;";
+
+        public DbSet<Тур> Тур {  get; set; }
+
+
+    }
+}
